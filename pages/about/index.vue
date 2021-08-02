a5d71fd5ccd0501170d1b9ec70ce881eeb104431
Nasser Aldossary <37676287+Nsz12@users.noreply.github.com>
2021-08-02 12:10:41 +0300

    Recipes
    simple nuxt app

---
+++ pages/about/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>About the site</h1>
+</template>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="recipes">
+        <Recipe
+        thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVRGkcPMvFhzwutW5GpUE4VgqFOIZoqpYz-w&usqp=CAU"
+        title="title"
+        previewText="some nice preview"
+        />
+        <Recipe
+        thumbnail="https://www.iamexpat.de/sites/default/files/styles/ogimage_thumb/public/german-food-cuisine-dishes.jpg?itok=aC12c0xI"
+        title="title"
+        previewText="some nice preview"
+        />
+        <Recipe
+        thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRq-2h3mMpVqmS7Qcjctdkpiza6D573nocrw&usqp=CAU"
+        title="title"
+        previewText="some nice preview"
+        />
+    </section>
+</template>
+<script scoped>
+import Recipe from '@/components/Recipe'
+export default {
+    components: {
+        Recipe
+    }
+}
+</script>
+<style scoped>
+.recipes{
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+
+</style>
